b9f4f37d23ef4394bea7a0150898b6d2f8c234cb
Mayowa Olufeko <mayfeko@gmail.com>
2024-08-06 15:09:38 +0100

    Linking single article page to article card, linking article card component to article list

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { ArticlesList } from "./components/ArticlesList";
 import { Routes, Route } from "react-router-dom"
 import { SingleArticle } from "./components/SingleArticle";
+import { ArticlesCard } from "./components/ArticlesCard";
 
 function App() {
   return (
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import { fetchArticleById } from "../api";
+import { Link } from "react-router-dom";
 
 export const SingleArticle = () => {
   const { article_id } = useParams();
@@ -18,6 +19,7 @@ export const SingleArticle = () => {
 
   return (
     <>
+    <Link to='/'>Go Back</Link>
     <img src={article.article_img_url} />
     <p>{article.author}</p>
     <p>{article.title} </p>

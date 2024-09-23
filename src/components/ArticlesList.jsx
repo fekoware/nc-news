3fa30820be2f4ad2354a6a75ebe1ab8b14c5a36c
Mayowa Olufeko <mayfeko@gmail.com>
2024-09-23 11:46:21 +0100

    updates

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -9,11 +9,17 @@ import { TopicsList } from "./TopicsList.jsx";
 import { useParams } from "react-router-dom";
 
 export const ArticlesList = () => {
+  const { topicSlug } = useParams();
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    fetchArticles()
+    setIsLoading(true);
+    const sortBy = searchParams.get("sort_by") || "created_at";
+    const order = searchParams.get("order") || "desc";
+
+    fetchArticles({ sort_by: sortBy, order: order  })
       .then((data) => {
         setArticles(data);
         console.log("hello")
@@ -21,6 +27,7 @@ export const ArticlesList = () => {
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   }, [topicSlug, searchParams]);
 
@@ -28,17 +35,88 @@ export const ArticlesList = () => {
     return <h2 className="loading"> Loading articles ...</h2>;
   }
 
+  const handleSortByChange = (event) => {
+    const sortBy = event.target.value;
+    setSearchParams({
+      sort_by: sortBy,
+      order: searchParams.get("order") || "desc",
+    });
+  };
+
+  const handleSortOrderChange = (event) => {
+    const order = event.target.value;
+    setSearchParams({
+      sort_by: searchParams.get("sort_by") || "created_at",
+      order: order,
+    });
+  };
+
   return (
     articles && (
       <div>
-        <TopicsList />
+        <form>
+          <label>Sort By: </label>
+          <select
+            value={searchParams.get("sort_by") || "created_at"}
+            onChange={handleSortByChange}
+          >
+            <option>Select One</option>
+            <option value="created_at">Date</option>
 
+            <option value="votes">Votes</option>
+          </select>
+
+          <label>Order: </label>
+          <select
+            value={searchParams.get("order") || "desc"}
+            onChange={handleSortOrderChange}
+          >
+            <option>Select One</option>
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </form>
+
+        <TopicsList />
         <ul>
-          {articles.map((article) => (
-            <ArticlesCard article={article} key={article.article_id} />
-          ))}
+          {articles.map((article) => {
+            return <ArticlesCard article={article} key={article.article_id} />;
+          })}
         </ul>
       </div>
     )
   );
 };
+
+/*
+
+sorting articles by queries
+  "author",
+  "topic",
+  "sort_by",
+  "order"
+
+order by ascending or descending
+
+  components
+  Articles List
+    states = queryParam, setQueryParam
+    searchParams
+
+  props
+
+  input
+
+  process
+
+  output
+
+  there will be a dropdown list of options to sort by with default of ascending
+  when a choice is pressed, onclick will handle the change to sort by the choice
+  useeffect will have the handlechange function inside
+
+  handleChange will sort the articles by the order and 
+
+
+
+*/

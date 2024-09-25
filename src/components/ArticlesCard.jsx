051dd7de2b147526083fe54ddeb7362a65b7b56b
Mayowa Olufeko <mayfeko@gmail.com>
2024-09-25 16:47:20 +0100

    styling for homepage. header, sort, order, and initial styling of article card complete

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,30 +1,26 @@
-import { Link } from "react-router-dom"
-import "../css/ArticlesCard.css"
+import { Link } from "react-router-dom";
+
 import { useParams } from "react-router-dom";
 
-export const ArticlesCard = ({article}) => {
+export const ArticlesCard = ({ article }) => {
   const { topicSlug } = useParams();
-    
 
   return (
-    
-    <li className='article-card'key={article.article_id}>
+    <div class=' hover:underline my-4'>
+      <li  key={article.article_id}>
         
-    <Link to={`/articles/${article.topic}/${article.article_id}`}>
-   <img src={article.article_img_url} />
-   <p>{article.author}</p>
-   <p>{article.title} </p>
-   <p>{article.topic}</p>
-   <p>{article.votes} likes </p>
-   <p>{article.category} </p>
-   <p>{article.created_at} </p>
-   <p>{article.comment_count} comments </p>
-   </Link>
- </li>
-
-    
-  )
-
-  
-
-}
+          <img class={"transition duration-300 ease-in-out filter brightness-100 hover:brightness-110 border-b-4 border-transparent hover:border-red-500 ${isClicked ? 'border-red-500' : 'border-transparent'} "}  src={article.article_img_url} />
+          <p>{article.author}</p>
+          <p>{article.title} </p>
+          <p>{article.topic}</p>
+          <p>{article.votes} likes </p>
+          <p>{article.category} </p>
+          <p>{article.created_at} </p>
+          <p>{article.comment_count} comments </p>
+          <Link to={`/articles/${article.topic}/${article.article_id}`}>
+          <button  className="bg-red-500 w-full text-white px-4 py-2  transition-opacity duration-200 ease-in-out hover:opacity-80 active:opacity-100" > View</button>
+        </Link>
+      </li>
+    </div>
+  );
+};

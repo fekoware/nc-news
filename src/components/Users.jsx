66fa6deac4ebe6311e32bd3746c6cf7c2262babd
Mayowa Olufeko <mayfeko@gmail.com>
2024-09-27 16:16:50 +0100

    styling

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,7 +4,13 @@ import { postComment } from "../api";
 import { useState } from "react";
 import { useEffect } from "react";
 
-export const Users = ({ username, setUsername,article, comments, setComments }) => {
+export const Users = ({
+  username,
+  setUsername,
+  article,
+  comments,
+  setComments,
+}) => {
   const [users, setUsers] = useState([]);
   const [commentPost, setCommentPost] = useState("");
   const [commentInput, setCommentInput] = useState("");
@@ -21,7 +27,6 @@ export const Users = ({ username, setUsername,article, comments, setComments }) 
 
   const handleUserChange = (event) => {
     setUsername(event.target.value);
-
   };
 
   const handleCommentChange = (event) => {
@@ -32,11 +37,9 @@ export const Users = ({ username, setUsername,article, comments, setComments }) 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-   
-
-    postComment(article.article_id, username, commentInput)
+    postComment(username, commentInput, article.article_id)
       .then((data) => {
-
+        console.log(data);
         return setComments((currComments) => {
           return [data.comment, ...currComments];
         });
@@ -48,9 +51,11 @@ export const Users = ({ username, setUsername,article, comments, setComments }) 
   };
 
   return (
-    <div>
+    <div class='py-4'>
       <form onSubmit={handleSubmit}>
-        <label> Choose User:</label>
+      <div></div>
+        <label class='font-bold'> Choose A User:</label>
+
         <select value={username} onChange={handleUserChange}>
           {users.map((user) => {
             return (
@@ -62,13 +67,21 @@ export const Users = ({ username, setUsername,article, comments, setComments }) 
         </select>
         <br></br>
 
-        <label> Comment Form</label>
+        <label class="py-2"> Post A Comment as {username}</label>
         <input
-          type="text"
-          onChange={handleCommentChange}
-          value={commentInput}
-        ></input>
-        <button type="submit">Post Comment</button>
+       class="w-full border-2 border-green-300 focus:border-green-800 hover:border-green-500 focus:ring-2 focus:ring-green-500 focus:outline-none transition ease-in-out duration-300 pl-2"
+       type="text"
+       onChange={handleCommentChange}
+       value={commentInput}
+        >
+          
+        </input>
+        <button
+          class=" w-full bg-green-700  text-white px-4 py-2  transition-opacity duration-200 ease-in-out hover:opacity-80 active:opacity-100 my-2"
+          type="submit"
+        >
+          Post Comment
+        </button>
       </form>
     </div>
   );

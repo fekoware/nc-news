0818c240f73ce6af3f6032389efebbb001a875d2
Mayowa Olufeko <mayfeko@gmail.com>
2024-09-27 16:16:19 +0100

    styling single article, with responsice layout. more styling needed, in line with split of user and comment form components

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,31 +1,35 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { fetchArticleById, updateVotesByArticleId } from "../api";
 import { Link } from "react-router-dom";
 import { ArticleCommentsList } from "./ArticleCommentsList";
-import "../css/SingleArticle.css";
 import { Users } from "./Users";
+import { TopicsList } from "./TopicsList";
+import { formatDistanceToNow } from "date-fns";
+
 
 export const SingleArticle = () => {
   const { article_id, topicSlug } = useParams();
   const [article, setArticle] = useState("");
   const [count, setCount] = useState(0);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [comments, setComments] = useState("");
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("jessjelly");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchArticleById(article_id).then((data) => {
+      setIsLoading(false)
       setArticle(data);
     });
-  }, [article_id]);
+  }, [article_id, comments]);
 
   const incrementCount = (increment) => {
     setCount((currCount) => {
       setError(null);
       return currCount + increment;
     });
+
     updateVotesByArticleId(article.article_id, increment).catch((err) => {
       //if unable to increment conditional rendering
       setCount((currCount) => {
@@ -35,53 +39,95 @@ export const SingleArticle = () => {
     });
   };
 
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <h1 className="text-xl font-bold">Loading Article...</h1>
+        <div className="w-16 h-16 border-8 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
-    <div class='flex w-full justify-center'> 
-
-    <div class=' flex flex-wrap w-1/2 items-center justify-center '> 
-      <Link to={`/articles/${topicSlug}`}>Go Back</Link>
-
-
-      <p class='flex w-full  justify-center py-2 text-3xl font-bold'>{article.title} </p>
-
-
-
-      <img src={article.article_img_url} />
-      <p>{article.author}</p>
-      <p>{article.topic}</p>
-      <p>{article.votes + count} likes </p>
-
-      <div>
-        <button onClick={() => incrementCount(1)}>Like Article</button>
-        <button onClick={() => incrementCount(-1)}>Remove Like</button>
-        {error ? <p>{error}</p> : null}
+    <div className="grid grid-cols-1 w-full justify-center">
+      <div className="flex items-center justify-center w-full p-5 bg-red-500">
+        <TopicsList />
       </div>
 
-      <p>{article.category} </p>
-      <p>{article.created_at} </p>
-      <p>{article.comment_count} comments </p>
+      <div className="grid grid-cols-1 lg:w-1/2 mx-auto">
+        <Link className="w-full text-left" to={`/articles/${topicSlug}`}>
+          <p className="text-lg">Back</p>
+        </Link>
+
+        <p className="text-3xl font-bold">{article.title}</p>
+        <p class="font-bold text-2xl pb-1">{article.author}</p>
+
+        <img
+          className="max-w-3xl w-full h-auto"
+          src={article.article_img_url}
+          alt={article.title}
+        />
+        {/*  */}
+
+        
+
+        <p className="text-left py-2 text-lg">{article.body}</p>
+
+        <div className="flex w-full py-1">
+          {/*  */}
+          <div class="flex flex-wrap w-full  justify-start">
+            <p class="flex w-full">{article.comment_count} comments</p>
+            <p class="flex w-full ">{article.votes + count} likes</p>
+          </div>
+          {/*  */}
+          <div className="flex flex-wrap justify-end w-full">
+            <p class="flex w-full justify-end">{article.topic}</p>
+            <p className="flex flex-wrap justfify-end"> {article.created_at}</p>
+          </div>
+          {/*  */}
+        </div>
+
+        <div className="flex flex-col items-start">
+          <button
+            onClick={() => incrementCount(1)}
+            class="bg-green-700 w-full text-white px-4 py-2  transition-opacity duration-200 ease-in-out hover:opacity-80 active:opacity-100 my-2"
+          >
+            Like Article
+          </button>
+          <button
+            class="bg-red-500 w-full text-white px-4 py-2  transition-opacity duration-200 ease-in-out hover:opacity-80 active:opacity-100"
+            onClick={() => incrementCount(-1)}
+          >
+            Remove Like
+          </button>
+          {error && <p>{error}</p>}
+        </div>
+
+
+
+        <Users 
+          username={username}
+          setUsername={setUsername}
+          article={article}
+          key={article.article_id}
+          comments={comments}
+          setComments={setComments}
+        />
+        
+        
+
+        <div >
+          <p class='w-full font-bold text-lg'> Comments</p>
+          <ArticleCommentsList
+            username={username}
+            setUsername={setUsername}
+            comments={comments}
+            setComments={setComments}
+          />
+        </div>
+
       
-      <p>{article.body} </p>
-
-      <Users
-        username={username}
-        setUsername={setUsername}
-        article={article}
-        key={article.article_id}
-        comments={comments}
-        setComments={setComments}
-      />
-      <ArticleCommentsList
-        username={username}
-        setUsername={setUsername}
-        comments={comments}
-        setComments={setComments}
-      />
-
-      <p> View other articles - links of other articles to be placed here</p>
       </div>
-
-      </div>
+    </div>
   );
 };
-

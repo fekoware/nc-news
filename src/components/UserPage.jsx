0761d45fdc9578cdddab88a6db97df5e87bf9f1c
Mayowa Olufeko <mayfeko@gmail.com>
2024-10-03 17:19:34 +0100

    initial component creation

---
+++ src/components/UserPage.jsx
@@ -0,0 +1,12 @@
+import { fetchUsers } from "../api"
+
+export const UserPage = ({username}) => {
+
+
+    //fetch all users
+
+    // iterate through users. if username selected matches
+    // the user, display the user details in the database
+
+
+}

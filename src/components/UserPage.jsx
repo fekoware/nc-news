bb527d52a9e37d975bd4ec574c3c58fba366c5b4
Mayowa Olufeko <mayfeko@gmail.com>
2024-10-10 15:32:00 +0100

    styling the user page which shows user details

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -4,11 +4,16 @@ import { useEffect } from "react";
 
 export const UserPage = ({ username, setUsername }) => {
   const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+
   console.log(username, "username");
 
   useEffect(() => {
     fetchUsers()
       .then((data) => {
+        setIsLoading(false);
         console.log(data);
         data.map((user) => {
           if (username === user.username) {
@@ -23,20 +28,36 @@ export const UserPage = ({ username, setUsername }) => {
       })
       .catch((err) => {
         console.log(err, "an error");
+        setIsError(true);
       });
   }, [username]);
 
-  //fetch all users
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen space-y-4">
+        <h1 className="text-xl font-bold">Loading User...</h1>
+        <div className="w-16 h-16 border-8 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
 
-  // iterate through users. if username selected matches
-  // the user, display the user details in the database
-  console.log(user.name, "hello");
+  if (isError) {
+    return (
+      <div class="flex flex-col justify-center items-center h-screen space-y-4">
+        <h1 class="text-xl font-bold">
+          Error loading users, refresh this page
+        </h1>
+      </div>
+    );
+  }
+
+
   return (
 
-    <div className="flex text-center justify-center items-center">
-  <p>{user.name}</p>
-  <p>{user.username}</p>
-  <img src={user.avatar_url} alt={`${user.name}'s avatar`} />
+    <div className="grid grid-cols-1 pt-10 justify-items-center">
+  <p class='text-3xl font-bold'>{user.name}</p>
+  <p class='text-2xl font-bold'> {user.username}</p>
+  <img class='flex w-1/5 h-auto pt-10' src={user.avatar_url} alt={`${user.name}'s avatar`} />
   </div>
 
   )
